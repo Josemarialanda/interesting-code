a42e892cc47e1d7d6eb1b68ff9a18e1faaa3752a
josemarialanda <josemaria.landa@gmail.com>
2023-04-01 21:17:26 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Some code I might use later";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = inputs:
+    let
+      overlay = final: prev: {
+        haskell = prev.haskell // {
+          packageOverrides = hfinal: hprev:
+            prev.haskell.packageOverrides hfinal hprev // {
+              interesting-code = hfinal.callCabal2nix "interesting-code" ./. { };
+            };
+        };
+        interesting-code = final.haskell.lib.compose.justStaticExecutables final.haskellPackages.interesting-code;
+      };
+      perSystem = system:
+        let
+          pkgs = import inputs.nixpkgs { inherit system; overlays = [ overlay ]; };
+          hspkgs = pkgs.haskellPackages;
+        in
+        {
+          devShell = hspkgs.shellFor {
+            withHoogle = true;
+            packages = p: [ p.interesting-code ];
+            buildInputs = [
+              hspkgs.cabal-install
+              hspkgs.haskell-language-server
+              hspkgs.hlint
+              hspkgs.ormolu
+              pkgs.bashInteractive
+            ];
+          };
+          defaultPackage = pkgs.interesting-code;
+        };
+    in
+    { inherit overlay; } // 
+      inputs.flake-utils.lib.eachDefaultSystem perSystem;
+}
