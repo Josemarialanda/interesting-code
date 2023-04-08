d8093a34af977e693f5be83fab765f255e8a4210
josemarialanda <josemaria.landa@gmail.com>
2023-04-08 15:16:13 +0100

    pushed from ipad pro 11

--- src/Test.hs
+++ src/Test.hs
@@ -1,1 +1,8 @@
 module Test where
+
+quickSort :: Ord a => [a] -> [a]
+quickSort [] = []
+quickSort (x:xs) = quickSort smaller ++ [x] ++ quickSort larger
+  where
+    smaller = [a | a <- xs, a <= x]
+    larger  = [b | b <- xs, b > x]
